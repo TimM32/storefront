8cc4b71f99b3c95c491449d2413f5a1d73a12603
Tim Maupin <timmaupin@live.com>
2023-07-07 12:04:10 -0500

    adding in the id for products

--- src/Store/Products/index.jsx
+++ src/Store/Products/index.jsx
@@ -1,12 +1,14 @@
-import { createStore, combineReducers } from 'redux';
-import categoriesReducer from './Categories';
-import productsReducer from './Products';
-
-const rootReducer = combineReducers({
-  categories: categoriesReducer,
-  products: productsReducer,
-});
-
-const store = createStore(rootReducer);
-
-export default store;
+const initialState = {
+    products: [
+        {id:1, category: 'weapons', name: 'Mace', description: 'Crystal base, leather handled', price: 999.99},
+        {id:2, category: 'weapons', name: 'Greatsword', description: 'Two Handed sword, platinum', price: 799.99},
+        {id:3, category: 'foods', name: 'Steak', description: '12oz T-Bone', price: 69.99},
+        {id:4, category: 'foods', name: 'Banana', description: 'Dozen Bananas', price: 12.99},
+        {id:5, category: 'foods', name: 'Bread', description: 'Loaf of sourdough', price: 3.99},
+        {id:6, category: 'electronics', name: 'Ipad', description: 'Touchscreen tablet', price: 59.99},
+        {id:7, category: 'electronics', name: 'TV', description: '60 inch HD TV', price: 599.99},
+        {id:8, category:},
+        {id:9, category:},
+        {id:10, category:},
+    ],
+}

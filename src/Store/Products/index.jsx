69f13df6fc483f6e51805e389566fac9e1bb018e
Tim Maupin <timmaupin@live.com>
2023-07-07 12:10:22 -0500

    finish the products and reducers to retrieve product

--- src/Store/Products/index.jsx
+++ src/Store/Products/index.jsx
@@ -7,8 +7,24 @@ const initialState = {
         {id:5, category: 'foods', name: 'Bread', description: 'Loaf of sourdough', price: 3.99},
         {id:6, category: 'electronics', name: 'Ipad', description: 'Touchscreen tablet', price: 59.99},
         {id:7, category: 'electronics', name: 'TV', description: '60 inch HD TV', price: 599.99},
-        {id:8, category:},
-        {id:9, category:},
-        {id:10, category:},
+        {id:8, category: 'clothing', name: 'Shorts', description: 'Cargo shorts', price: 29.99},
+        {id:9, category: 'clothing', name: 'T-Shirt', description: 'Casual short sleeve shirt', price: 13.99},
+        {id:10, category: 'clothing', name: 'Coat', description: 'Long fully button down fancy coat', price: 49.99},
     ],
-}
+};
+
+export const getProductsFromCategory = (state, category) => {
+    if(category) {
+        return state.products.filter((product) => product.category === category.name);
+    }
+    return state.products;
+};
+
+const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default productReducer;

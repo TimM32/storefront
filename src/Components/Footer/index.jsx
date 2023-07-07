8d88dd114694a1db4618d25090193011ead5f1e8
Tim Maupin <timmaupin@live.com>
2023-07-07 11:29:25 -0500

    slowly building out header and footer, getting the structure set up before bringing in images/outside links

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,7 @@
+// Might need to import something here...come back to check later
+
+const Footer = () => {
+    return <footer>&copy; 2023 Virtual Store. All rights reserved.</footer>;
+  };
+  
+  export default Footer;
